44ac13f95d61be5173f1069d0009c4c7cbaec4ff
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2019-02-13 22:41:41 -0300

    Data/DAO

--- Data/FileExtensionDAO.cs
+++ Data/FileExtensionDAO.cs
@@ -7,5 +7,25 @@ namespace FileSystemWatcherComponent.Data
 {
     class FileExtensionDAO
     {
+        //var connectionString = ConfigurationManager.ConnectionStrings["LiteDB"].ConnectionString;
+         using(var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+        {
+                    //DateTime dateTime = new DateTime();
+                    // simple access to Insert/Update/Upsert/Delete
+                    //db.Insert(new FilePathDTO { Path = "Table"});
+
+                    // Get customer collection
+                    var col = db.GetCollection<FilePathDTO>("customers");
+
+            // Create your new customer instance
+            var filePathDTO = new FilePathDTO
+            {
+                Path = "John Doe"
+            };
+
+            // Insert new customer document (Id will be auto-incremented)
+            col.Insert(filePathDTO);    
+       
+        }
     }
 }
--- Data/FilePathDAO.cs
+++ Data/FilePathDAO.cs
@@ -8,12 +8,23 @@ namespace FileSystemWatcherComponent.Data
     class FilePathDAO
     {
         //var connectionString = ConfigurationManager.ConnectionStrings["LiteDB"].ConnectionString;
-
-        using(var db = new LiteDatabase(@"C:\database.db"))
+         using(var db = new LiteDatabase(@"C:\Temp\MyData.db"))
         {
             //DateTime dateTime = new DateTime();
             // simple access to Insert/Update/Upsert/Delete
-            db.Insert(new FilePathDTO { Path = "Table"});
+            //db.Insert(new FilePathDTO { Path = "Table"});
+
+            // Get customer collection
+            var col = db.GetCollection<FilePathDTO>("customers");
+
+            // Create your new customer instance
+            var filePathDTO = new FilePathDTO
+            {
+                Path = "John Doe"
+            };
+	
+            // Insert new customer document (Id will be auto-incremented)
+            col.Insert(filePathDTO);    
        
         }
     }
